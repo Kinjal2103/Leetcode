34e9fb4a561d39a95bc0ce68c96c13f951338ff1
Kinjal Agarwal <kinjalagarwal2103@gmail.com>
2025-08-20 22:19:20 +0530

    Create Pow(x,n).cpp

---
+++ Pow(x,n).cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/powx-n/
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1;
+        long b=n;
+        if(b<0){
+            x=1/x;
+            b=-b;
+        }
+        while(b){
+            if(b%2==1) ans*=x;
+            x*=x;
+            b/=2;
+        }
+        return ans;
+    }
+};
